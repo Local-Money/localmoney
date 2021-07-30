dfce85c5d78d53fa4c89a10643ca88a31c6dbe33
Samuel Barbosa <sambarbosaa@gmail.com>
2021-07-30 16:31:10 -0500

    Removed serde renaming

--- contracts/offer/src/currencies.rs
+++ contracts/offer/src/currencies.rs
@@ -2,7 +2,6 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
 pub enum FiatCurrency {
     AED,
     AFN,
