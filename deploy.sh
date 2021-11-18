99fc27967b6f46e6b5627a38ffc30fc9d668027d
sambarboza <sambarbosaa@gmail.com>
2021-11-18 18:24:06 -0500

    Added deploy script.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Local-Terra/Local-Terra.github.io.git master:gh-pages
+
+cd -
+
