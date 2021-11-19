4a73bbe5c4b7157e58396e28ec9a66b0109fb446
Davi Rodrigues <davirds07@gmail.com>
2021-11-19 17:05:39 -0500

    Mock currency layer response (#34)

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-import axios from 'axios'
+// import axios from 'axios'
 
 import transactions from './modules/transactions'
 
@@ -14,10 +14,10 @@ export default createStore({
   },
   actions: {
     async fetchUsdRates({ commit }) {
-      const URL =
-        'http://www.apilayer.net/api/live?access_key=78bd873aa48cdf5de03ce0b650cda9eb&format=1&currencies=COP,BRL,ARS'
-      const rates = await axios.get(URL)
-      commit('setUsdRates', rates.data.quotes)
+      // const URL = 'http://www.apilayer.net/api/live?access_key=78bd873aa48cdf5de03ce0b650cda9eb&format=1&currencies=COP,BRL,ARS'
+      // const rates = await axios.get(URL)
+      const responseMock = { "USDBRL": 5.557904, "USDCOP": 3935, "USDARS": 100.360503 };
+      commit('setUsdRates', responseMock)
     },
   },
   mutations: {
