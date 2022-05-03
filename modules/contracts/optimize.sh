a0e6c26ebf1c9bf88e59279c0c1764639962fb2d
Samuel Barbosa <sambarbosaa@gmail.com>
2022-05-03 17:38:02 -0500

    upload-inst 2022-05-03-22_38_02_UTC

--- modules/contracts/optimize.sh
+++ modules/contracts/optimize.sh
@@ -6,8 +6,8 @@ docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   cosmwasm/rust-optimizer:0.12.6
 
-DATE=$(date -u +"%Y-%m-%d-%H_%M_%S_UTC") # Date in UTC
-echo "$DATE"
+#DATE=$(date -u +"%Y-%m-%d-%H_%M_%S_UTC") # Date in UTC
+#echo "$DATE"
 
-git commit -a -m "build $DATE" || true
-git tag "build_$DATE"
+#git commit -a -m "build $DATE" || true
+#git tag "build_$DATE"
