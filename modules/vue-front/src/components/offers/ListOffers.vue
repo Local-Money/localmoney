265177bf523cc8d2ff246f3dda34685485e0be33
Gabriel Brettas <gabrielbrettas@gmail.com>
2022-05-04 19:12:22 -0300

    Move select style to element.scss

--- modules/vue-front/src/components/offers/ListOffers.vue
+++ modules/vue-front/src/components/offers/ListOffers.vue
@@ -31,6 +31,7 @@
                 <label for="currency">Currency (FIAT)</label>
                 <select
                     name="currency"
+                    class="bg-surface"
                     id="currency"
                     v-model="fiatCurrency"
                     @change="fetchOffers({ fiatCurrency, offerType })"
@@ -254,30 +255,6 @@ export default defineComponent({
         font-size: 12px;
         color: $gray600;
         margin-bottom: 8px;
-    }
-
-    select {
-        width: 100%;
-        max-width: 200px;
-        background-color: $surface;
-        border-radius: 8px;
-        border: 1px solid $border;
-        font-family: "Poppins", sans-serif;
-        font-size: 14px;
-        font-weight: 600;
-        color: $base-text;
-        padding: 10px 16px;
-        appearance: none;
-    }
-
-    @media only screen and (max-width: 550px) {
-        margin-left: 0;
-        max-width: none;
-
-        select {
-            max-width: none;
-            height: 48px;
-        }
     }
 }
 
