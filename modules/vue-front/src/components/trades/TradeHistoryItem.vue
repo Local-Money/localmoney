66b7a216258d170be1887dd5f2db40a89ee202b7
Davi Rodrigues <davirds07@gmail.com>
2022-05-06 17:13:53 -0300

    apply offer rate on trade history and detail

--- modules/vue-front/src/components/trades/TradeHistoryItem.vue
+++ modules/vue-front/src/components/trades/TradeHistoryItem.vue
@@ -1,83 +1,89 @@
 <template>
-  <div class="wrap-table-item">
-    <div class="col-1">
-      <p>{{ currentDate }}</p>
+    <div class="wrap-table-item">
+        <div class="col-1">
+            <p>{{ currentDate }}</p>
+        </div>
+        <div class="col-2">
+            <p>{{ tradeType }}</p>
+        </div>
+        <div class="col-3">
+            <p>{{ formatAmount(this.tradeInfo.trade.ust_amount) }} UST</p>
+        </div>
+        <div class="col-4">
+            <p>{{ fiatAmountStr }}</p>
+        </div>
+        <div class="col-5 trader">
+            <p>{{ formatAddress(counterparty) }}</p>
+        </div>
+        <div class="col-6">
+            <p>{{ tradeState(this.tradeInfo.trade.state) }}</p>
+        </div>
     </div>
-    <div class="col-2">
-      <p>{{ tradeType }}</p>
-    </div>
-    <div class="col-3">
-      <p>{{ formatAmount(this.tradeInfo.trade.ust_amount) }} UST</p>
-    </div>
-    <div class="col-4">
-      <p>{{ fiatAmountStr }}</p>
-    </div>
-    <div class="col-5 trader">
-      <p>{{ formatAddress(counterparty) }}</p>
-    </div>
-    <div class="col-6">
-      <p>{{ tradeState(this.tradeInfo.trade.state) }}</p>
-    </div>
-  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import {
-  formatAddress,
-  formatAmount,
-  formatFiatAmount,
-  formatTradeState,
-  formatDate,
-} from "@/shared";
-import {mapActions, mapGetters} from "vuex";
-
-export default defineComponent({
-  name: "TradeHistoryItem",
-  props: ["tradeAddr"],
-  methods: {
-    ...mapActions([
-      "fetchUsdRates",
-    ]),
-    formatAmount,
     formatAddress,
+    formatAmount,
+    formatFiatAmount,
     formatTradeState,
     formatDate,
-    tradeState: function(state) {
-      return formatTradeState(state);
+    calculateFiatPriceByRate,
+} from "@/shared";
+import { mapActions, mapGetters } from "vuex";
+
+export default defineComponent({
+    name: "TradeHistoryItem",
+    props: ["tradeAddr"],
+    methods: {
+        ...mapActions(["fetchUsdRates"]),
+        formatAmount,
+        formatAddress,
+        formatTradeState,
+        formatDate,
+        tradeState: function(state) {
+            return formatTradeState(state);
+        },
     },
-  },
-  mounted() {
-    this.fetchUsdRates()
-  },
-  computed: {
-    ...mapGetters(["walletAddress", "getUsdRate", "getTradeInfo"]),
-    tradeInfo: function() {
-      return this.getTradeInfo(this.$props.tradeAddr);
+    mounted() {
+        this.fetchUsdRates();
     },
-    currentDate: function() {
-      let date = new Date(this.tradeInfo.trade.created_at * 1000);
-      return this.formatDate(date);
+    computed: {
+        ...mapGetters(["walletAddress", "getUsdRate", "getTradeInfo"]),
+        tradeInfo: function() {
+            return this.getTradeInfo(this.$props.tradeAddr);
+        },
+        currentDate: function() {
+            let date = new Date(this.tradeInfo.trade.created_at * 1000);
+            return this.formatDate(date);
+        },
+        fiatCurrency: function() {
+            return this.tradeInfo.offer.fiat_currency;
+        },
+        fiatPriceByRate: function() {
+            return calculateFiatPriceByRate(
+                this.getUsdRate(this.fiatCurrency),
+                this.tradeInfo.offer.rate,
+            );
+        },
+        fiatAmountStr: function() {
+            const fiatAmount = formatFiatAmount(
+                (this.tradeInfo.trade.ust_amount / 1000000) *
+                    this.fiatPriceByRate,
+            );
+            return `${this.fiatCurrency} ${fiatAmount}`;
+        },
+        tradeType: function() {
+            return this.tradeInfo.offer.offer_type === "buy" ? "Buy" : "Sell";
+        },
+        counterparty: function() {
+            const trade = this.tradeInfo.trade;
+            return this.walletAddress === trade.seller
+                ? trade.buyer
+                : trade.seller;
+        },
     },
-    fiatCurrency: function() {
-      return this.tradeInfo.offer.fiat_currency;
-    },
-    fiatAmountStr: function() {
-      const fiatAmount = formatFiatAmount((this.tradeInfo.trade.ust_amount / 1000000) * this.getUsdRate(this.fiatCurrency));
-      return `${this.fiatCurrency} ${fiatAmount}`;
-    },
-    tradeType: function() {
-      return this.tradeInfo.offer.offer_type === "buy"
-        ? "Buy"
-        : "Sell";
-    },
-    counterparty: function() {
-      const trade = this.tradeInfo.trade;
-      return this.walletAddress === trade.seller
-        ? trade.buyer
-        : trade.seller;
-    },
-  },
 });
 </script>
 
@@ -85,17 +91,17 @@ export default defineComponent({
 @import "../../style/tokens.scss";
 
 .wrap-table-item {
-  display: flex;
-  flex-direction: row;
-  padding: 16px;
+    display: flex;
+    flex-direction: row;
+    padding: 16px;
 
-  p {
-    font-size: 14px;
-    font-weight: $regular;
-  }
+    p {
+        font-size: 14px;
+        font-weight: $regular;
+    }
 
-  .trader {
-    color: $primary;
-  }
+    .trader {
+        color: $primary;
+    }
 }
 </style>
