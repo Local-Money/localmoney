8d99c82cf01a8a4fd456b63174f66cd1c7c7cc33
Davi Rodrigues <davirds07@gmail.com>
2022-05-19 13:30:12 -0300

    fix: remove unused import

--- modules/contracts/packages/protocol/examples/trading_incentives_schema.rs
+++ modules/contracts/packages/protocol/examples/trading_incentives_schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema_with_title, remove_schemas};
 use schemars::schema_for;
-use localterra_protocol::trading_incentives::{ExecuteMsg, QueryMsg, InstantiateMsg, Cw20HookMsg};
+use localterra_protocol::trading_incentives::{ExecuteMsg, QueryMsg, InstantiateMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
