21f10e4c07cfa25c7242006635463c7ed0097c69
Davi Rodrigues <davirds07@gmail.com>
2022-07-14 15:28:15 -0300

    chore: delete old front

--- vue-front/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './style/reset.scss'
-
-const app = createApp(App)
-
-app.use(store).use(router).mount('#app')
