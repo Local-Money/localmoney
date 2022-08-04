b7f67cc413c12cbba4ad4555b3f81e72a2b697af
Gabriel Brettas <gabrielbrettas@gmail.com>
2022-08-04 19:30:11 -0300

    Added new states

--- app/src/ui/components/commons/NotificationWidget.vue
+++ app/src/ui/components/commons/NotificationWidget.vue
@@ -19,7 +19,7 @@ function toggleWidget() {
   <div class="wrap-widget">
     <div class="wrap-btn" @click="toggleWidget">
       <div class="badge">
-        <p>2</p>
+        <p>4</p>
       </div>
       <div class="btn">
         <svg viewBox="0 0 24 24" fill="none">
@@ -50,21 +50,7 @@ function toggleWidget() {
           <li class="item">
             <svg class="icon" viewBox="0 0 24 24" fill="none">
               <path
-                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-                stroke="inherit"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12.01 8L12.01 12"
-                stroke="inherit"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12.01 16L12 16"
+                d="M20 6L9 17L4 12"
                 stroke="inherit"
                 stroke-width="2"
                 stroke-linecap="round"
@@ -72,10 +58,10 @@ function toggleWidget() {
               />
             </svg>
             <div class="wrap">
-              <p class="status">You have a new trade request</p>
+              <p class="status">Trade finished successfully</p>
               <p class="addr">from kujira6...6yp193</p>
             </div>
-            <p class="timestamp">2h ago</p>
+            <p class="timestamp">15s ago</p>
           </li>
           <li class="item">
             <svg class="icon" viewBox="0 0 24 24" fill="none">
@@ -102,10 +88,10 @@ function toggleWidget() {
               />
             </svg>
             <div class="wrap">
-              <p class="status">You have a new trade request</p>
+              <p class="status">Buyer is waiting for funds</p>
               <p class="addr">from kujira6...6yp193</p>
             </div>
-            <p class="timestamp">2h ago</p>
+            <p class="timestamp">35m ago</p>
           </li>
           <li class="item">
             <svg class="icon" viewBox="0 0 24 24" fill="none">
@@ -132,7 +118,7 @@ function toggleWidget() {
               />
             </svg>
             <div class="wrap">
-              <p class="status">You have a new trade request</p>
+              <p class="status">The seller is waiting for your payment</p>
               <p class="addr">from kujira6...6yp193</p>
             </div>
             <p class="timestamp">2h ago</p>
@@ -271,7 +257,7 @@ function toggleWidget() {
     background-color: $surface;
     border: 1px solid $border;
     border-radius: 8px;
-    box-shadow: 0px 2px 25px rgba(0, 0, 0, 0.05);
+    box-shadow: 0px 2px 25px rgba(0, 0, 0, 0.2);
 
     .header {
       display: flex;
