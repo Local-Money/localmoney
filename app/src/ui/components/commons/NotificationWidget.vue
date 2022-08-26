83d798972eee0cf8b883d3168f7e5ac3c729a999
gabs <1717634+gabs-local@users.noreply.github.com>
2022-08-26 15:34:19 -0300

    Adjustment to the empty state

--- app/src/ui/components/commons/NotificationWidget.vue
+++ app/src/ui/components/commons/NotificationWidget.vue
@@ -1,27 +1,27 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
-import useNotificationSystem from "~/notification/Notification";
-import { useClientStore } from "~/stores/client";
-import { formatAddress, timeSince } from "~/shared";
-import type { Notification } from "~/stores/notification";
+import { useRouter } from 'vue-router'
+import useNotificationSystem from '~/notification/Notification'
+import { useClientStore } from '~/stores/client'
+import { formatAddress, timeSince } from '~/shared'
+import type { Notification } from '~/stores/notification'
 
-const notification = useNotificationSystem();
-const client = useClientStore();
-const router = useRouter();
-const isConnected = computed(() => client.userWallet.isConnected);
+const notification = useNotificationSystem()
+const client = useClientStore()
+const router = useRouter()
+const isConnected = computed(() => client.userWallet.isConnected)
 
-const widgetActive = ref(false);
+const widgetActive = ref(false)
 function toggleWidget() {
-  widgetActive.value = !widgetActive.value;
+  widgetActive.value = !widgetActive.value
 }
 
 async function showTrade(n: Notification) {
-  await notification.readNotification(n);
+  await notification.readNotification(n)
   await router.push({
-    name: "TradeDetail",
+    name: 'TradeDetail',
     params: { id: n.id },
-  });
-  toggleWidget();
+  })
+  toggleWidget()
 }
 </script>
 
@@ -53,18 +53,11 @@ async function showTrade(n: Notification) {
     <div v-if="widgetActive" class="widget">
       <div class="header">
         <p class="title">Notifications</p>
-        <p class="mark-read" @click="notification.readAllNotifications()">
-          Mark all as read
-        </p>
+        <p class="mark-read" @click="notification.readAllNotifications()">Mark all as read</p>
       </div>
       <div class="content">
         <ul v-if="notification.notificationCount() > 0">
-          <li
-            v-for="n in notification.notifications()"
-            :key="`${n.id}_${n.state}`"
-            class="item"
-            @click="showTrade(n)"
-          >
+          <li v-for="n in notification.notifications()" :key="`${n.id}_${n.state}`" class="item" @click="showTrade(n)">
             <svg class="icon" viewBox="0 0 24 24" fill="none">
               <path
                 d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
@@ -73,20 +66,8 @@ async function showTrade(n: Notification) {
                 stroke-linecap="round"
                 stroke-linejoin="round"
               />
-              <path
-                d="M12.01 8L12.01 12"
-                stroke="inherit"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12.01 16L12 16"
-                stroke="inherit"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
+              <path d="M12.01 8L12.01 12" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M12.01 16L12 16" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
             <div class="wrap">
               <p class="status">{{ n.message }}</p>
@@ -95,8 +76,8 @@ async function showTrade(n: Notification) {
             <p class="timestamp">{{ timeSince(n.time) }}</p>
           </li>
         </ul>
-        <div v-else class="item">
-          <p>No notifications yet</p>
+        <div v-else class="empty-state">
+          <p>Nothing new here.</p>
         </div>
       </div>
     </div>
@@ -105,7 +86,7 @@ async function showTrade(n: Notification) {
 </template>
 
 <style lang="scss" scoped>
-@import "../../style/tokens.scss";
+@import '../../style/tokens.scss';
 
 .wrap-widget {
   display: flex;
@@ -238,6 +219,11 @@ async function showTrade(n: Notification) {
           font-size: 12px;
           color: $gray900;
         }
+      }
+      .empty-state {
+        padding: 24px;
+        font-size: 14px;
+        color: $gray700;
       }
     }
   }
