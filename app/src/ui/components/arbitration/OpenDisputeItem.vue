65082315d4e7a5ab6cb485f3ed292ef8ccff6ef2
gabs <1717634+gabs-local@users.noreply.github.com>
2022-09-14 16:08:25 -0300

    fix: dispute card item (#85)
    * Hard run of the OpenDisputeItem component
    
    * chore: adjusted layout of the dispute status
    
    * chore: layout adjustments
    
    * fix: responsive
    
    Co-authored-by: gabs <gabs@localmoney.io>

--- app/src/ui/components/arbitration/OpenDisputeItem.vue
+++ app/src/ui/components/arbitration/OpenDisputeItem.vue
@@ -1,52 +1,71 @@
 <script setup lang="ts">
-import type { TradeInfo } from '~/types/components.interface'
+// import type { TradeInfo } from '~/types/components.interface'
 
-const props = defineProps<{ dispute: TradeInfo }>()
+// const props = defineProps<{ dispute: TradeInfo }>()
 </script>
 
 <template>
   <div class="dispute-wrap">
-    <div class="time"><p>45m ago</p></div>
-    <div class="info">
-      <div class="wrap">
-        <p class="offer-type">{{ props.dispute.offer.offer_type }}</p>
-      </div>
-
-      <div class="divider" />
-      <div class="wrap">
-        <p class="label">Reward (estimated)</p>
-        <!-- TO-DO Get Trade Rate -->
-        <p class="rate">$12.826 LOCAL</p>
-      </div>
-    </div>
-
-    <div class="dispute-status">
-      <div class="wrap">
-        <!-- <p class="peer">Maker</p> -->
-        <p class="address">kujira7...o8lkv</p>
-      </div>
+    <div class="time">
       <div class="icon">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 12H19" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
           <path
-            d="M12 5L19 12L12 19"
+            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
             stroke="inherit"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
+          <path d="M12 6V12L16 14" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
       </div>
-      <div class="wrap">
-        <!-- <p class="peer">Taker</p> -->
-        <p class="address">kujira7...o8lkv</p>
+      <p>45m ago</p>
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="info">
+      <div class="dispute-status">
+        <div class="wrap-peer">
+          <p class="peer">Maker</p>
+          <p class="address">kujira7...o8lkv</p>
+        </div>
+        <div class="wrap-offer-type">
+          <div class="icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 12H19" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path
+                d="M12 5L19 12L12 19"
+                stroke="inherit"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <p class="offer-type">
+            Selling to
+            <!-- {{ props.dispute.offer.offer_type }} -->
+          </p>
+        </div>
+        <div class="wrap-peer">
+          <p class="peer">Taker</p>
+          <p class="address">kujira7...o8lkv</p>
+        </div>
       </div>
     </div>
 
-    <div class="btn">
-      <router-link :to="`/trade/${dispute.trade.id}`">
-        <button class="primary bg-gray300" type="button">view</button>
-      </router-link>
+    <div class="divider"></div>
+
+    <div class="wrap-cta">
+      <div class="reward">
+        <p class="label">Estimated rewards</p>
+        <!-- TO-DO Get Trade Rate -->
+        <p class="rate">$12.826 LOCAL</p>
+      </div>
+      <!-- <router-link :to="`/trade/${dispute.trade.id}`"> -->
+      <button class="primary bg-gray300" type="button">view</button>
+      <!-- </router-link> -->
     </div>
   </div>
 </template>
@@ -58,98 +77,143 @@ const props = defineProps<{ dispute: TradeInfo }>()
   display: flex;
   justify-content: space-between;
   align-items: center;
-  gap: 16px;
+  gap: 40px;
 
   @media only screen and (max-width: $mobile) {
     flex-direction: column;
-    gap: 24px;
+    gap: 32px;
   }
-  .time p {
-    font-size: 16px;
-    font-weight: $semi-bold;
-    color: $primary;
-    background-color: $gray150;
-    border-radius: 8px;
-    padding: 8px 16px;
+  .time {
+    display: flex;
+    align-items: center;
+    margin-left: 8px;
+
+    @media only screen and (max-width: $mobile) {
+      background-color: $gray150;
+      padding: 8px 16px;
+      border-radius: 8px;
+    }
+
+    .icon svg {
+      vertical-align: middle;
+      stroke: $gray700;
+    }
+    p {
+      font-size: 16px;
+      font-weight: $semi-bold;
+      color: $primary;
+      margin-left: 16px;
+    }
   }
 
   .info {
     display: flex;
+    justify-content: space-between;
     align-items: center;
     gap: 24px;
 
-    .divider {
-      height: 40px;
-      width: 1px;
-      background-color: $border;
+    @media only screen and (max-width: $mobile) {
+      width: 100%;
+      flex-direction: column;
+      gap: 8px;
     }
 
-    .wrap {
+    .dispute-status {
+      display: flex;
+      align-items: center;
+      gap: 40px;
+
+      @media only screen and (max-width: $mobile) {
+        width: 100%;
+        justify-content: space-between;
+        flex-direction: row;
+        gap: 16px;
+      }
+
+      .wrap-peer {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 16px;
+        background-color: $gray150;
+        border-radius: 8px;
+        padding: 8px 16px;
+
+        @media only screen and (max-width: $mobile) {
+          flex-direction: column;
+          gap: 4px;
+          padding: 8px 8px;
+        }
+
+        .peer {
+          font-size: 16px;
+        }
+        .address {
+          font-size: 12px;
+          color: $gray600;
+        }
+      }
+
+      .wrap-offer-type {
+        display: flex;
+        gap: 8px;
+        align-items: center;
+        flex-direction: row-reverse;
+
+        @media only screen and (max-width: $mobile) {
+          flex-direction: column-reverse;
+        }
+        .offer-type {
+          font-size: 14px;
+          font-weight: $semi-bold;
+          color: $gray900;
+        }
+        .icon svg {
+          vertical-align: middle;
+          stroke: $primary;
+        }
+      }
+    }
+  }
+  .divider {
+    height: 40px;
+    width: 1px;
+    background-color: $border;
+
+    @media only screen and (max-width: $mobile) {
+      display: none;
+    }
+  }
+
+  .wrap-cta {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+
+    @media only screen and (max-width: $mobile) {
+      width: 100%;
+      justify-content: space-between;
+      border-top: 1px solid $border;
+      padding-top: 24px;
+    }
+
+    .reward {
       display: flex;
       flex-direction: column;
+      text-align: right;
+
+      @media only screen and (max-width: $mobile) {
+        text-align: left;
+      }
       .label {
         margin-bottom: 4px;
         font-size: 12px;
         color: $gray600;
       }
-      .offer-type {
-        font-size: 14px;
-        font-weight: $semi-bold;
-        color: $gray900;
-      }
-
-      .limit,
-      .rate,
-      .content {
+      .rate {
         font-size: 15px;
         color: $gray700;
       }
-    }
-
-    @media only screen and (max-width: $mobile) {
-      flex-direction: column;
-      align-items: flex-start;
-      gap: 8px;
-      background-color: $gray150;
-      padding: 8px 16px;
-      border-radius: 8px;
-
-      .divider {
-        display: none;
-      }
-      .wrap {
-        width: 100%;
-        flex-direction: row;
-        align-items: flex-end;
-        gap: 24px;
-        justify-content: space-between;
-      }
-    }
-  }
-
-  .dispute-status {
-    display: flex;
-    align-items: center;
-    gap: 24px;
-
-    .wrap {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
-      .peer {
-        font-size: 14px;
-        margin-bottom: 4px;
-      }
-      .address {
-        font-size: 14px;
-        background-color: $gray150;
-        border-radius: 8px;
-        padding: 8px 16px;
-      }
-    }
-    .icon svg {
-      stroke: $primary;
     }
   }
 }
