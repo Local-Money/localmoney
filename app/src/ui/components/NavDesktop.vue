a35a239485302fdf64dfad6a64e6fb37da279226
gabs <gabs@localmoney.io>
2022-09-30 16:19:57 -0300

    chore: changed nav icon and added navMobile access

--- app/src/ui/components/NavDesktop.vue
+++ app/src/ui/components/NavDesktop.vue
@@ -43,15 +43,17 @@ const isArbitrator = computed(() => {
 
       <li v-if="isArbitrator" class="item">
         <router-link to="/arbitration">
-          <svg class="icon-24" viewBox="0 0 24 24" fill="none">
+          <svg
+            class="icon-24"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
-              d="M2 3H8C9.06087 3 10.0783 3.42143 10.8284 4.17157C11.5786 4.92172 12 5.93913 12 7V21C12 20.2044 11.6839 19.4413 11.1213 18.8787C10.5587 18.3161 9.79565 18 9 18H2V3Z"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M22 3H16C14.9391 3 13.9217 3.42143 13.1716 4.17157C12.4214 4.92172 12 5.93913 12 7V21C12 20.2044 12.3161 19.4413 12.8787 18.8787C13.4413 18.3161 14.2044 18 15 18H22V3Z"
+              d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
+              stroke="inherit"
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
