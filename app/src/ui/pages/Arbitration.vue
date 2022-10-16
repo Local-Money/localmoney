edf1de5693828c9ba6617909421cd59dad5b600f
gabs <gabs@localmoney.io>
2022-10-16 18:33:47 -0300

    fix: Adjusted pages to take into account the fixed navbar height

--- app/src/ui/pages/Arbitration.vue
+++ app/src/ui/pages/Arbitration.vue
@@ -2,13 +2,19 @@
 
 <template>
   <main class="page">
-    <h3>Open Disputes</h3>
+    <div class="wrap-title">
+      <h3>Open Disputes</h3>
+    </div>
     <ListDisputes />
   </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../style/pages.scss';
+
+.wrap-title {
+  display: flex;
+}
 
 h3 {
   margin: 32px 0;
