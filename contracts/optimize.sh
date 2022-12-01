2eed84cb2f7e80cc796eec18397f1cade6db01fd
davirds <davirds07@gmail.com>
2022-12-01 13:04:19 -0300

    chore: change back the optimize.sh

--- contracts/optimize.sh
+++ contracts/optimize.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -e
 
-#docker run --rm -v "$(pwd)":/code \
-#  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-#  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-#  cosmwasm/rust-optimizer:0.12.9
-
-# ARM
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer-arm64:0.12.8
+  cosmwasm/rust-optimizer:0.12.9
+
+# ARM
+#docker run --rm -v "$(pwd)":/code \
+#  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+#  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+#  cosmwasm/workspace-optimizer-arm64:0.12.8
