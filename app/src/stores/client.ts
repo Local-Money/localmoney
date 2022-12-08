c015e20efcdc472cb853f2e4a35fe3c1f2ef24b9
davirds <davirds07@gmail.com>
2022-12-08 19:55:13 -0300

    change  to

--- app/src/stores/client.ts
+++ app/src/stores/client.ts
@@ -130,7 +130,7 @@ export const useClientStore = defineStore({
     async unarchiveOffer(updateOffer: PatchOffer) {
       this.loadingState = LoadingState.show('Archiving Offer...')
       try {
-        updateOffer.state = OfferState.PAUSED
+        updateOffer.state = OfferState.paused
         await this.client.updateOffer(updateOffer)
         await this.fetchMyOffers()
       } catch (e) {
