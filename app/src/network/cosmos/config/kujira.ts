28f34d069a56b9319b668732099c6823833facae
davirds <davirds07@gmail.com>
2022-12-13 11:43:36 -0300

    Testnet Release (#134)
    * setup front to release new version on testnet
    
    * fix: divider should disappear when the timer ends

--- app/src/network/cosmos/config/kujira.ts
+++ app/src/network/cosmos/config/kujira.ts
@@ -12,15 +12,15 @@ export const KUJIRA_TESTNET_CONFIG: CosmosConfig = {
 }
 
 export const KUJIRA_TESTNET_HUB_INFO: HubInfo = {
-  hubAddress: 'kujira16u6leuaassucdvx6awuzxypp0r7wdusl8rmm72f689f8qcvd2p4q5pznz3',
+  hubAddress: 'kujira1vyy7f920fm4rrcwgzac4p0grez2e5ckd4zjq7v9ahumcpre4ft5qu7agt7',
   hubConfig: {
-    offer_addr: 'kujira1nhr7kxps88vvy6tz6sx7d4x0j0gnl95e5s05feengutvmcl7nyws7sg9ce',
-    trade_addr: 'kujira1y8artn4enfulxlau9ycv33e8xrsy6a29n20dl6pfhxgflfqd005qp0vj6n',
+    offer_addr: 'kujira1h7g5u3lr5j74hcsfrrls4anauv7u72a94cppyscl32rqm5lzz7psu4438v',
+    trade_addr: 'kujira13lg50j2hak2quzax2xvt6sjk6j7s73ljzvvrv95cjjcur95z62cstl3qqj',
     price_addr: 'kujira14e8dc9x2vqltcs28wwu96jtnymrse80cmmj9nc37x4ymh7xjmrjqavedqd',
-    profile_addr: 'kujira190ea5krlq7gvdalsaay8u2ww23v9y6qs9qvyljh2s0x6h8qpkvwq4sgc7c',
+    profile_addr: 'kujira15felz5ycysspattde8sl26wqw0m9ghzgg7dajm6edt96a03sjw8qxk92fq',
     local_market_addr: 'kujira1chejx4qqtvwxy6684yrsmf6pylancxqhk3vsmtleg5ta3zrffljq4xf685',
     local_denom: {
-      native: 'factory/kujira12w0ua4eqnkk0aahtnjlt6h3dhxael6x25s507w/local',
+      native: 'ukuji',
     },
   },
 }
