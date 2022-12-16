4e4df2048b654655ba0309915673e6a5f719e71d
davirds <davirds07@gmail.com>
2022-12-16 16:11:20 -0300

    add trades pagination test

--- app/tests/pagination.spec.ts
+++ app/tests/pagination.spec.ts
@@ -4,8 +4,11 @@ import dotenv from 'dotenv'
 import { setupProtocol } from './utils'
 import type { TestCosmosChain } from './network/TestCosmosChain'
 import makerSecrets from './fixtures/maker_secrets.json'
-import { FiatCurrency, OfferOrder, OfferType } from '~/types/components.interface'
-import 'isomorphic-fetch'
+import takerSecrets from './fixtures/taker_secrets.json'
+import { encryptDataMocked } from './helper'
+import adminSecrets from './fixtures/admin_secrets.json'
+import type { PatchOffer } from '~/types/components.interface'
+import { FiatCurrency, OfferOrder, OfferState, OfferType, TradeState } from '~/types/components.interface'
 
 dotenv.config()
 Object.assign(global, { TextEncoder, TextDecoder })
@@ -18,11 +21,13 @@ const args = {
 }
 const limit = 3
 
+let adminClient: TestCosmosChain
 let takerClient: TestCosmosChain
 let makerClient: TestCosmosChain
 jest.setTimeout(60 * 1000)
 beforeAll(async () => {
   const result = await setupProtocol()
+  adminClient = result.adminClient
   takerClient = result.takerClient
   makerClient = result.makerClient
 })
@@ -30,6 +35,8 @@ beforeAll(async () => {
 describe('offers pagination', () => {
   it(`maker must have at least ${limit} offers`, async () => {
     let myOffers = await makerClient.fetchMyOffers(limit)
+    myOffers = myOffers.filter((offer) => offer.offer.state !== OfferState.archived)
+    const ownerContact = await encryptDataMocked(makerSecrets.publicKey, 'maker001')
     if (myOffers.length < limit) {
       for (let i = 0; i < limit - myOffers.length; i++) {
         await makerClient.createOffer({
@@ -38,7 +45,7 @@ describe('offers pagination', () => {
           min_amount: '100000',
           max_amount: '1000000',
           offer_type: args.offerType,
-          owner_contact: 'maker001',
+          owner_contact: ownerContact,
           owner_encryption_key: makerSecrets.publicKey,
           rate: (100 - i).toString(),
         })
@@ -69,4 +76,79 @@ describe('offers pagination', () => {
       expect(offers[offers.length - 1].offer.id).toBe(previousId)
     }
   })
+})
+
+describe('trades pagination', () => {
+  it('should have an arbitrator available', async () => {
+    const fiat = args.fiatCurrency
+    let arbitrators = await adminClient.fetchArbitrators()
+    if (arbitrators.find((arbitrator) => arbitrator.fiat === fiat) === undefined) {
+      await adminClient.newArbitrator({
+        arbitrator: adminClient.getWalletAddress(),
+        fiat,
+        encryption_key: adminSecrets.publicKey,
+      })
+      arbitrators = await adminClient.fetchArbitrators()
+    }
+    expect(arbitrators.filter((arb) => arb.fiat === fiat).length).toBeGreaterThan(0)
+  })
+  it(`maker must have at least ${limit} trades`, async () => {
+    let trades = await makerClient.fetchTrades()
+    if (trades.length < limit) {
+      const offers = await takerClient.fetchOffers(args, 1)
+      const offerResponse = offers[0]
+      const profileTakerContact = await encryptDataMocked(takerSecrets.publicKey, 'taker001')
+      const takerContact = await encryptDataMocked(offerResponse.profile.encryption_key!, 'taker001')
+      for (let i = 0; i < limit - trades.length; i++) {
+        await takerClient.openTrade({
+          amount: offerResponse.offer.min_amount,
+          offer_id: offerResponse.offer.id,
+          taker: takerClient.getWalletAddress(),
+          profile_taker_contact: profileTakerContact,
+          profile_taker_encryption_key: takerSecrets.publicKey,
+          taker_contact: takerContact,
+        })
+      }
+      trades = await makerClient.fetchTrades()
+    }
+    expect(trades.length).toBeGreaterThanOrEqual(limit)
+  })
+  it('maker should be able to paginate the list of trades', async () => {
+    let trades = await makerClient.fetchTrades(limit)
+    for (let i = 0; i < trades.length - 1; i++) {
+      const previousTradeId = trades[trades.length - 2].trade.id
+      const lastTradeId = trades[trades.length - 1].trade.id
+      trades = await makerClient.fetchTrades(limit, lastTradeId)
+      expect(trades[trades.length - 1].trade.id).toBe(previousTradeId)
+    }
+  })
+  it('taker should be able to paginate the list of trades', async () => {
+    let trades = await takerClient.fetchTrades(limit)
+    for (let i = 0; i < trades.length - 1; i++) {
+      const previousTradeId = trades[trades.length - 2].trade.id
+      const lastTradeId = trades[trades.length - 1].trade.id
+      trades = await takerClient.fetchTrades(limit, lastTradeId)
+      expect(trades[trades.length - 1].trade.id).toBe(previousTradeId)
+    }
+  })
+})
+
+afterAll(async () => {
+  // close all open trades in the trades pagination test
+  let trades = await makerClient.fetchTrades(limit)
+  trades = trades.filter((trade) => trade.trade.state === TradeState.request_created)
+  for (const tradeInfo of trades) {
+    await makerClient.cancelTradeRequest(tradeInfo.trade.id)
+  }
+
+  // archive all offers created in the offers pagination test
+  let offers = await makerClient.fetchMyOffers(limit)
+  offers = offers.filter((offer) => offer.offer.state !== OfferState.archived)
+  for (const offerResponse of offers) {
+    const offer: PatchOffer = {
+      ...offerResponse.offer,
+      state: OfferState.archived,
+    }
+    await makerClient.updateOffer(offer)
+  }
 })
