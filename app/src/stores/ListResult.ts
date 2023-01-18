a1c6557abaee862e0bfac4571c5adcac87bab223
davirds <davirds07@gmail.com>
2023-01-18 18:22:29 -0300

    adjust ListResult to be abble to pagination

--- app/src/stores/ListResult.ts
+++ app/src/stores/ListResult.ts
@@ -1,14 +1,18 @@
 import type { ChainError } from '~/network/chain-error'
 
 export class ListResult<T> {
-  readonly loading: boolean
   readonly data: T[]
   readonly error: ChainError | null
+  readonly loading: boolean
+  private loadingMore: boolean
+  private enableLoadMore: boolean
 
-  private constructor(loading: boolean, data: T[], error: ChainError | null) {
+  private constructor(loading: boolean, data: T[], error: ChainError | null, enableLoadMore = false) {
     this.data = data
     this.error = error
     this.loading = loading
+    this.loadingMore = false
+    this.enableLoadMore = enableLoadMore
   }
 
   isSuccess(): boolean {
@@ -24,11 +28,29 @@ export class ListResult<T> {
   }
 
   isLoading(): boolean {
-    return this.loading
+    return this.loading && this.data.length === 0
   }
 
-  static success<T>(data: T[]) {
-    return new ListResult<T>(false, data, null)
+  isLoadingMore(): boolean {
+    return this.loadingMore
+  }
+
+  showLoadMore(): boolean {
+    return this.enableLoadMore
+  }
+
+  setLoadingMore() {
+    this.loadingMore = true
+  }
+
+  addMoreItems(items: T[], pageLimit = 30) {
+    this.data.push(...items)
+    this.enableLoadMore = items.length === pageLimit
+    this.loadingMore = false
+  }
+
+  static success<T>(data: T[], pageLimit = 30) {
+    return new ListResult<T>(false, data, null, data.length === pageLimit)
   }
 
   static error<T>(error: ChainError) {
