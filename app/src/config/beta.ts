dfa856634b22ef60c310c6592755d4b1075741b9
davirds <davirds07@gmail.com>
2023-01-27 19:48:23 -0300

    change beta maker validation to work only for mainnet

--- app/src/config/beta.ts
+++ app/src/config/beta.ts
@@ -4,5 +4,5 @@ import makers from '~/config/makers.json'
 
 export function isBetaMaker(addr: Addr, chainClient: ChainClient): boolean {
   // TODO Changes the validation to validate if it is the Mainnent (pending of the LOCAL-1004)
-  return chainClient === ChainClient.dev ? makers.includes(addr) : true
+  return chainClient === ChainClient.kujiraMainnet ? makers.includes(addr) : true
 }
