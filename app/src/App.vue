cb8d024b1de7f4951afe782b939d6ad8df6121d9
davirds <davirds07@gmail.com>
2023-01-27 17:51:09 -0300

    use localstorage to save beta acceptance

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+import { useLocalStorage } from '@vueuse/core'
 import BetaPopUp from './ui/components/commons/BetaPopUp.vue'
 import { ChainClient } from '~/network/Chain'
 import { useClientStore } from '~/stores/client'
@@ -9,9 +10,9 @@ import './ui/style/reset.scss'
 import 'vue-toastification/dist/index.css'
 
 const client = useClientStore()
+const betaAccepted = useLocalStorage('beta-accept', false)
 client.setClient(ChainClient.dev) // required to properly init chain
 const loading = computed(() => client.loadingState)
-const showWelcome = ref(true)
 
 useHead({
   title: 'Local Money - TESTNET',
@@ -29,7 +30,7 @@ useHead({
   <RouterView />
   <!-- Loading Modal -->
   <ModalLoading :loading="loading" />
-  <BetaPopUp :show="showWelcome" @accept="showWelcome = false" />
+  <BetaPopUp :show="!betaAccepted" @accept="betaAccepted = true" />
 </template>
 
 <style lang="scss">
