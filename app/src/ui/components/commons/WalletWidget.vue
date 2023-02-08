b00b47be78ebf60a2f8894585d158ceab8529b3e
gabs <gabs@localmoney.io>
2023-02-08 16:54:33 -0300

    chore: add address link to finder

--- app/src/ui/components/commons/WalletWidget.vue
+++ app/src/ui/components/commons/WalletWidget.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { useClientStore } from '~/stores/client'
+import { formatAddress } from '~/shared'
+import { ChainClient } from '~/network/Chain'
 // import { useClientStore } from '~/stores/client'
 // import { microDenomToDenom } from '~/utils/denom'
 // import { formatAmount } from '~/shared'
@@ -10,6 +13,17 @@
 //   return microDenomToDenom(denom)
 // }
 
+const client = useClientStore()
+const userWallet = computed(() => client.userWallet)
+
+const finder = computed(() => {
+  if (client.chainClient === ChainClient.kujiraMainnet) {
+    return `kaiyo-1/address/${userWallet.value.address}`
+  } else {
+    return `harpoon-4/address/${userWallet.value.address}`
+  }
+})
+
 const widgetActive = ref(false)
 function toggleWidget() {
   widgetActive.value = !widgetActive.value
@@ -20,7 +34,34 @@ defineExpose({ toggleWidget })
 <template>
   <div v-if="widgetActive" class="widget">
     <div class="wrap card">
-      <p class="title">My Wallet</p>
+      <div class="header">
+        <div class="inner">
+          <p class="title">My Wallet</p>
+          <p class="address">{{ formatAddress(userWallet.address) }}</p>
+        </div>
+        <div class="actions">
+          <a :href="`https://finder.kujira.app/${finder}`" target="_blank">
+            <svg
+              class="icon-24"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11"
+                stroke="inherit"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path d="M15 3H21V9" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M10 14L21 3" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </a>
+        </div>
+      </div>
       <!-- <div class="balance">
         <p class="label">{{ formatDenom(receivedBalance.denom) }}</p>
         <p class="balance">{{ formatAmount(receivedBalance.amount) }}</p>
@@ -108,6 +149,7 @@ defineExpose({ toggleWidget })
   position: absolute;
   right: 0;
   top: 64px;
+  z-index: $z-level-3;
 
   .wrap {
     min-width: 350px;
@@ -115,10 +157,28 @@ defineExpose({ toggleWidget })
     flex-direction: column;
     gap: 24px;
 
-    .title {
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
       padding-bottom: 16px;
       margin-bottom: 8px;
       border-bottom: 1px solid $border;
+      .inner {
+        .title {
+        }
+        .address {
+          font-size: 14px;
+          color: $gray700;
+        }
+      }
+      .actions {
+        svg {
+          width: 20px;
+          height: 20px;
+          stroke: $gray700;
+        }
+      }
     }
 
     .balance {
