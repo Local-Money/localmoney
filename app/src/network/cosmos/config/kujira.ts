6a29a68f456b2fa533293fe51f66c728aeeb3b85
davirds <davirds07@gmail.com>
2023-03-09 16:21:05 -0300

    change rpc and lcd back to the mintthemoon

--- app/src/network/cosmos/config/kujira.ts
+++ app/src/network/cosmos/config/kujira.ts
@@ -28,8 +28,8 @@ export const KUJIRA_TESTNET_HUB_INFO: HubInfo = {
 export const KUJIRA_MAINNET_CONFIG: CosmosConfig = {
   chainId: 'kaiyo-1',
   chainName: 'Kujira',
-  lcdUrl: 'https://lcd.kaiyo.kujira.setten.io',
-  rpcUrl: 'https://rpc.kaiyo.kujira.setten.io/',
+  lcdUrl: 'https://lcd-kujira.mintthemoon.xyz/',
+  rpcUrl: 'https://rpc-kujira.mintthemoon.xyz/',
   addressPrefix: 'kujira',
   coinDenom: 'KUJI',
   coinMinimalDenom: 'ukuji',
